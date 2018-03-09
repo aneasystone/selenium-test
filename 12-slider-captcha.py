da2d646f2ce10a40117e985551ac96927cf1b2a3
aneasystone <aneasystone@gmail.com>
2018-03-09 13:53:12 +0800

    Add slider captcha example

--- 12-slider-captcha.py
+++ 12-slider-captcha.py
@@ -2,73 +2,13 @@
 # -*- coding: utf-8 -*-
 # Created by aneasystone on 2018/3/2
 from PIL import Image, ImageChops
-import requests, io
-
-url = "https://static.geetest.com/pictures/gt/73f2074d1/73f2074d1.webp"
-image_file = io.BytesIO(requests.get(url).content)
-im = Image.open(image_file)
-
-url_bg = "https://static.geetest.com/pictures/gt/73f2074d1/bg/72a6e6d79.webp"
-image_file_bg = io.BytesIO(requests.get(url_bg).content)
-im_bg = Image.open(image_file_bg)
-
-# im.save('D:/b.jpeg')
-# im.show()
-
-css = [
-    "-157px -58px",
-    "-145px -58px",
-    "-265px -58px",
-    "-277px -58px",
-    "-181px -58px",
-    "-169px -58px",
-    "-241px -58px",
-    "-253px -58px",
-    "-109px -58px",
-    "-97px -58px",
-    "-289px -58px",
-    "-301px -58px",
-    "-85px -58px",
-    "-73px -58px",
-    "-25px -58px",
-    "-37px -58px",
-    "-13px -58px",
-    "-1px -58px",
-    "-121px -58px",
-    "-133px -58px",
-    "-61px -58px",
-    "-49px -58px",
-    "-217px -58px",
-    "-229px -58px",
-    "-205px -58px",
-    "-193px -58px",
-    "-145px 0px",
-    "-157px 0px",
-    "-277px 0px",
-    "-265px 0px",
-    "-169px 0px",
-    "-181px 0px",
-    "-253px 0px",
-    "-241px 0px",
-    "-97px 0px",
-    "-109px 0px",
-    "-301px 0px",
-    "-289px 0px",
-    "-73px 0px",
-    "-85px 0px",
-    "-37px 0px",
-    "-25px 0px",
-    "-1px 0px",
-    "-13px 0px",
-    "-133px 0px",
-    "-121px 0px",
-    "-49px 0px",
-    "-61px 0px",
-    "-229px 0px",
-    "-217px 0px",
-    "-193px 0px",
-    "-205px 0px"
-    ]
+from numpy import array
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+from selenium.webdriver.support import expected_conditions as Expect
+import requests, io, re
 
 
 def convert_css_to_offset(px):
@@ -85,19 +25,104 @@ def convert_index_to_offset(index):
     y = row * 58
     return x, y, x + 10, y + 58
 
-#
-# 10*58 26/row => background image size = 260*116
-#
-captcha = Image.new('RGB', (260, 116))
-captcha_bg = Image.new('RGB', (260, 116))
-for i, px in enumerate(css):
-    offset = convert_css_to_offset(px)
-    region = im.crop(offset)
-    region_bg = im_bg.crop(offset)
-    offset = convert_index_to_offset(i)
-    captcha.paste(region, offset)
-    captcha_bg.paste(region_bg, offset)
-# captcha.show()
-# captcha_bg.show()
-diff = ImageChops.difference(captcha, captcha_bg)
-diff.show()
+
+def get_slider_offset_from_diff_image(diff):
+    im = array(diff)
+    width, height = diff.size
+    diff = []
+    for i in xrange(height):
+        for j in xrange(width):
+            # black is not only (0,0,0)
+            if im[i, j, 0] > 15 or im[i, j, 1] > 15 or im[i, j, 1] > 15:
+                diff.append(j)
+                break
+    return min(diff)
+
+
+def get_slider_offset(image_url, image_url_bg, css):
+    image_file = io.BytesIO(requests.get(image_url).content)
+    im = Image.open(image_file)
+    image_file_bg = io.BytesIO(requests.get(image_url_bg).content)
+    im_bg = Image.open(image_file_bg)
+    # im.show()
+    # im_bg.show()
+
+    # 10*58 26/row => background image size = 260*116
+    captcha = Image.new('RGB', (260, 116))
+    captcha_bg = Image.new('RGB', (260, 116))
+    for i, px in enumerate(css):
+        offset = convert_css_to_offset(px)
+        region = im.crop(offset)
+        region_bg = im_bg.crop(offset)
+        offset = convert_index_to_offset(i)
+        captcha.paste(region, offset)
+        captcha_bg.paste(region_bg, offset)
+    diff = ImageChops.difference(captcha, captcha_bg)
+    # captcha.show()
+    # captcha_bg.show()
+    # diff.show()
+    diff.save('D:/diff.png')
+    return get_slider_offset_from_diff_image(diff)
+
+
+def get_image_css(images):
+    css = []
+    for image in images:
+        position = get_image_position_from_style(image.get_attribute("style"))
+        css.append(position)
+    return css
+
+
+def get_image_url_from_style(style):
+    match = re.match('background-image: url\("(.*?)"\); background-position: (.*?);', style)
+    return match.group(1)
+
+
+def get_image_position_from_style(style):
+    match = re.match('background-image: url\("(.*?)"\); background-position: (.*?);', style)
+    return match.group(2)
+
+
+def get_slice_offset(slice):
+    style = slice.get_attribute("style")
+    match = re.search('background-image: url\("(.*?)"\);', style)
+    url = match.group(1)
+    image_file = io.BytesIO(requests.get(url).content)
+    im = Image.open(image_file)
+    im.save('D:/slice.png')
+    return get_slider_offset_from_diff_image(im)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--start-maximized")
+browser = webdriver.Chrome(
+    executable_path="./drivers/chromedriver.exe",
+    chrome_options=chrome_options
+)
+browser.get('https://account.ch.com/NonRegistrations-Regist')
+Wait(browser, 60).until(
+    Expect.visibility_of_element_located((By.CSS_SELECTOR, "div[data-target='account-login']"))
+)
+email = browser.find_element_by_css_selector("div[data-target='account-login']")
+email.click()
+
+Wait(browser, 60).until(
+    Expect.visibility_of_element_located((By.ID, "emailRegist"))
+)
+register = browser.find_element_by_id("emailRegist")
+register.click()
+
+slice = browser.find_element_by_class_name("gt_slice")
+slice_offset = get_slice_offset(slice)
+print(slice_offset)
+
+images = browser.find_elements_by_class_name("gt_cut_fullbg_slice")
+images_bg = browser.find_elements_by_class_name("gt_cut_bg_slice")
+image_url = get_image_url_from_style(images[0].get_attribute("style"))
+image_url_bg = get_image_url_from_style(images_bg[0].get_attribute("style"))
+css = get_image_css(images)
+offset = get_slider_offset(image_url, image_url_bg, css)
+print(offset)
+
+knob = browser.find_element_by_class_name("gt_slider_knob")
+# ActionChains(browser).drag_and_drop_by_offset(knob, offset - slice_offset, 0).perform()
+ActionChains(browser).click_and_hold(knob).move_by_offset(offset - slice_offset, 0).release().perform()
