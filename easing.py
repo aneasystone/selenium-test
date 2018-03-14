39161a1fbe8c2ff0d5e778b6c6d8ce91e54bbdbc
aneasystone <aneasystone@gmail.com>
2018-03-14 21:09:37 +0800

    Add easing functions

--- easing.py
+++ easing.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Created by aneasystone on 2018/3/14
 import numpy as np
+import math
 
 
 # https://github.com/gdsmith/jquery.easing/blob/master/jquery.easing.js
@@ -15,6 +16,39 @@ def ease_out_quad(x):
 
 def ease_out_quart(x):
     return 1 - pow(1 - x, 4)
+
+
+def ease_out_expo(x):
+    if x == 1:
+        return 1
+    else:
+        return 1 - pow(2, -10 * x)
+
+
+def ease_out_bounce(x):
+    n1 = 7.5625
+    d1 = 2.75
+    if x < 1 / d1 :
+        return n1 * x * x
+    elif x < 2 / d1:
+        x -= 1.5 / d1
+        return n1 * x*x + 0.75
+    elif x < 2.5 / d1:
+        x -= 2.25 / d1
+        return n1 * x*x + 0.9375
+    else:
+        x -= 2.625 / d1
+        return n1 * x*x + 0.984375
+
+
+def ease_out_elastic(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        c4 = (2 * math.pi) / 3
+        return pow(2, -10 * x) * math.sin((x * 10 - 0.75) * c4) + 1
 
 
 def get_tracks(distance, seconds, ease_func):
